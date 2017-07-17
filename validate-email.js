fa12cb7e086e32c2d3d9431dc75dbad9cfedd1ce
DanielKell <danielkell@Daniels-MacBook-Pro.local>
2017-07-17 10:44:53 -0700

    Changed header to be mobile first
    -Edited header to be mobile first
    -Spaced out buttons for carousel
    -Edited email validation message

--- validate-email.js
+++ validate-email.js
@@ -3,10 +3,10 @@ function validateForm() {
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
-        alert("Not a valid e-mail address");
+        alert("Please enter a valid e-mail address.");
         return false;
     }
     else {
-        alert("Thanks, got it!")
+        alert("Thanks for subscribing!")
     }
 }
